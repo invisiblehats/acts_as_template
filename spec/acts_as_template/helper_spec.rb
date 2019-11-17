0040e0c1a5c701265651dc7b6ceb44fbdae70aa8
Myk Klemme <mykklemme@gmail.com>
2019-11-17 07:58:44 -0800

    Add sample tests to run on CIs
    We just need something that will run & trigger a failure

---
+++ spec/acts_as_template/helper_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ActsAsTemplate, type: :model do
+  it 'this test succeeds' do
+    expect(true).to eq true
+  end
+
+  it 'this test fails' do
+    expect(true).to eq false
+  end
+end
