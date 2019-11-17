47c47e711ce4d848ab5d3eaedac29f55065bd264
Myk Klemme <mykklemme@gmail.com>
2019-11-17 08:02:49 -0800

    Remove failure test
    To get the tests to run we need to have the config in master and I dont 
    want the test to fail there

--- spec/acts_as_template/helper_spec.rb
+++ spec/acts_as_template/helper_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ActsAsTemplate, type: :model do
   it 'this test succeeds' do
     expect(true).to eq true
   end
-
-  it 'this test fails' do
-    expect(true).to eq false
-  end
 end
